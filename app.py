6013cf6922d77ebad2240d15ac7f2a628ec0b259
Suzana Guedes <vieirasuzana336@gmail.com>
2026-03-23 21:03:25 -0300

    Atualizando cod

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from controller.app import createUser
+from controller.auth import ativarConta, loginVendedor
 from service.service import getUser, deleteUser, lista_usuarios
 
 app = Flask(__name__)
@@ -8,17 +9,25 @@ app = Flask(__name__)
 def criarUsuario():
     return jsonify(createUser())
 
+@app.route('/ativar', methods=['POST'])
+def ativar():
+    return jsonify(ativarConta())
+
+@app.route('/login', methods=['POST'])
+def login():
+    return jsonify(loginVendedor())
+
 @app.route('/listar')
 def listarUsuarios():
     return jsonify(lista_usuarios)
 
-@app.route('/listar/<cpf>')
-def listarUsuario(cpf):
-    return jsonify(getUser(cpf))
+@app.route('/listar/<cnpj>')
+def listarUsuario(cnpj):
+    return jsonify(getUser(cnpj))
 
-@app.route('/deletar/<cpf>', methods=['DELETE'])
-def deletarUsuario(cpf):
-    return jsonify(deleteUser(cpf))
+@app.route('/deletar/<cnpj>', methods=['DELETE'])
+def deletarUsuario(cnpj):
+    return jsonify(deleteUser(cnpj))
 
 if __name__ == '__main__':
     app.run(debug=True)
