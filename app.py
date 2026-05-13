748c93f267c1e1c6d3f67824341a72df4a2f11c4
Maria Eduarda Gomes Romera <2402751@SALAS.AULAS>
2026-05-12 22:02:06 -0300

    Alterando funcionalidades

--- app.py
+++ app.py
@@ -66,6 +66,40 @@ def novaVenda():
 def verRelatorio():
     return jsonify(verDashboard())
 
+    from flask import render_template
+
+@app.route('/pagina/cadastro')
+def pagina_cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/pagina/ativacao')
+def pagina_ativacao():
+    return render_template('ativacao.html')
+
+@app.route('/pagina/login')
+def pagina_login():
+    return render_template('login.html')
+
+@app.route('/pagina/produtos')
+def pagina_produtos():
+    return render_template('produtos.html')
+
+@app.route('/pagina/cadastro_produto')
+def pagina_cadastro_produto():
+    return render_template('cadastro_produto.html')
+
+@app.route('/pagina/editar_produto')
+def pagina_editar_produto():
+    return render_template('editar_produto.html')
+
+@app.route('/pagina/vendas')
+def pagina_vendas():
+    return render_template('vendas.html')
+
+@app.route('/pagina/dashboard')
+def pagina_dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
